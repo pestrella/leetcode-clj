e7b1b49bb806f7e79ae9675834756c38dd09683d
p. estrella <estrella.programmer@gmail.com>
2020-06-22 16:22:06 +0100

    Add fibonacci implementation

---
+++ src/data_exercises_clj/fibonacci.clj
@@ -0,0 +1,31 @@
+(ns data-exercises-clj.fibonacci)
+
+(defn fib [n]
+  (condp = n
+    0 0
+    1 1
+    (+ (fib (dec n)) (fib (- n 2)))))
+
+(defn fib-seq [n]
+  (loop [i 0
+         s []]
+    (if (< i n)
+      (recur (inc i) (conj s (fib i)))
+      s)))
+
+(defn fib-seq'
+  "Lazy sequence fibonacci"
+  ([] (fib-seq' 0 1))
+  ([a b] (cons a (lazy-seq (fib-seq' b (+ a b))))))
+
+(defn fib'
+  "Fibonacci using laziness"
+  [n]
+  (nth (fib-seq') n))
+
+(comment
+  (fib-seq 10)
+  (take 10 (fib-seq'))
+  (time (fib 25))
+  (time (fib' 25))
+)
